cddac4212ab420b9bd7356879df7e32f9272959a
Matt Drollette <matt@drollette.com>
2012-11-18 20:49:55 -0600

    composer update

--- src/SOTB/CoreBundle/Form/Type/PurifiedTextareaType.php
+++ src/SOTB/CoreBundle/Form/Type/PurifiedTextareaType.php
@@ -20,7 +20,7 @@ class PurifiedTextareaType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->appendClientTransformer($this->purifierTransformer);
+        $builder->addViewTransformer($this->purifierTransformer);
     }
 
     public function getParent()
